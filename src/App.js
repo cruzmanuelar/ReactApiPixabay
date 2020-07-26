9e44780bb86b712bcbd3415c605d4de261670a7c
Manuel Cruz <cruzmanuelar@gmail.com>
2020-07-26 12:55:25 -0500

    Connect with API

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import React, { Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 //import "bootswatch/dist/litera/bootstrap.min.css";
 import Buscador from "./componentes/Buscador"
 
 class App extends Component{
+
+  state = {
+    termino: ""
+  }
+
+  consultarApi = () => {
+    const url = `https://pixabay.com/api/?key=15689473-9f661a18592fed1b7e8c3bcb3&q=${this.state.termino}`;
+  }
+
+  datosBusqueda = (termino) => {
+    this.setState({
+      termino
+    })
+  }
+
   render(){
     return (
       <div className="container-fluid text-light">
-        <div className="jumbotron">
-          <h3 className="bold text-center text-dark mb-4">Buscador de Imágenes</h3>
-          <Buscador/>
+        <div className="jumbotron text-dark">
+          <h3 className="bold text-center text-dark mb-4 textoB">Buscador de Imágenes</h3>
+          <Buscador datosBusqueda={this.datosBusqueda}/>
+          {this.state.termino}
         </div>
       </div>
     );
   }
 }
-  
+
+
 
 export default App;
--- src/componentes/Buscador.js
+++ src/componentes/Buscador.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 
 class Buscador extends Component {
 
+    busquedaRef = React.createRef();
+    obtenerDatos = (e) => {
+        e.preventDefault();
+
+        const termino = this.busquedaRef.current.value;
+        this.props.datosBusqueda(termino);
+    }
+
     render() { 
         return (
-            <form>
-                <div className="row">
+            <form onSubmit={this.obtenerDatos}>
+                <div className="row text-dark">
                     <div className="form-group col-md-8">
-                        <input type="text" className="form-control form-control-lg" placeholder="¿Qué desea buscar?"/>
+                        <input ref={this.busquedaRef} type="text" className="form-control textoB form-control-lg" placeholder="¿Qué desea buscar?"/>
                     </div>
                     <div className="form-group col-md-4">
                         <input type="submit" className="btn btn-success btn-lg btn-block rounded-0" value="Buscar"/>
