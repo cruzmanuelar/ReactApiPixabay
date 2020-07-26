d7129fbf6b8d2f50187894d81268a50b5f911bd4
Manuel Cruz <cruzmanuelar@gmail.com>
2020-07-26 13:22:18 -0500

    call images ok

--- src/App.js
+++ src/App.js
@@ -6,16 +6,25 @@ import Buscador from "./componentes/Buscador"
 class App extends Component{
 
   state = {
-    termino: ""
+    termino: "",
+    imagenes : []
   }
 
   consultarApi = () => {
-    const url = `https://pixabay.com/api/?key=15689473-9f661a18592fed1b7e8c3bcb3&q=${this.state.termino}`;
+    const termino = this.state.termino;
+    const url = `https://pixabay.com/api/?key=15689473-9f661a18592fed1b7e8c3bcb3&q=${termino}&per_page=30`;
+    console.log(url);
+
+    fetch(url)
+    .then(respuesta => respuesta.json())
+    .then(resultado => this.setState({imagenes: resultado.hits}))
   }
 
   datosBusqueda = (termino) => {
     this.setState({
       termino
+    },() => {
+      this.consultarApi();
     })
   }
 
