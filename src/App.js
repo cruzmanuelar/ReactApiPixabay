f64740578199cb482dabf4a1abbfe97335099377
Manuel Cruz <cruzmanuelar@gmail.com>
2020-07-26 15:16:38 -0500

    Insert images to page

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import React, { Component} from 'react';
 import './App.css';
 //import "bootswatch/dist/litera/bootstrap.min.css";
 import Buscador from "./componentes/Buscador"
+import Resultado from "./componentes/Resultado"
 
 class App extends Component{
 
   state = {
     termino: "",
-    imagenes : []
+    imagenes : [],
+    color: "red"
   }
 
   consultarApi = () => {
     const termino = this.state.termino;
     const url = `https://pixabay.com/api/?key=15689473-9f661a18592fed1b7e8c3bcb3&q=${termino}&per_page=30`;
-    console.log(url);
 
     fetch(url)
     .then(respuesta => respuesta.json())
@@ -34,7 +35,7 @@ class App extends Component{
         <div className="jumbotron text-dark">
           <h3 className="bold text-center text-dark mb-4 textoB">Buscador de Imágenes</h3>
           <Buscador datosBusqueda={this.datosBusqueda}/>
-          {this.state.termino}
+          <Resultado imagenes={this.state.imagenes}/>
         </div>
       </div>
     );
