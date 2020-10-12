986ed39cfdc7a6bfbd8e931f1651e4bef816dabd
Manuel Cruz <cruzmanuelar@gmail.com>
2020-10-11 20:54:01 -0500

    Placeholder_ComponenteBuscador

--- src/componentes/Buscador.js
+++ src/componentes/Buscador.js
@@ -15,7 +15,7 @@ class Buscador extends Component {
             <form onSubmit={this.obtenerDatos}>
                 <div className="row text-dark">
                     <div className="form-group col-md-8">
-                        <input ref={this.busquedaRef} type="text" className="form-control textoB form-control-lg" placeholder="¿Qué desea buscar?"/>
+                        <input ref={this.busquedaRef} type="text" className="form-control textoB form-control-lg" placeholder="Palabra clave"/>
                     </div>
                     <div className="form-group col-md-4">
                         <input type="submit" className="btn btn-success btn-lg btn-block rounded-0" value="Buscar"/>
